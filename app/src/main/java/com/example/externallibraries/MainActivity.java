746dbae3a15c07caf36bc72a2e91147590738bb3
Carl <a23carpa@student.his.se>
2024-05-07 14:44:08 +0200

    Set the imageView image through the external library picasso.

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String imageLink = "https://i5.walmartimages.com/seo/Nobleworks-Holy-Cow-Holy-Mackerel-Randy-McIlwaine-Humorous-Funny-Christmas-Card_60d713e0-8052-4199-9866-032437fc67cb.6a306c8ece31f61aa16b26c8d8548238.jpeg";
+
+        ImageView web = findViewById(R.id.webImage);
+        Picasso.get().load(imageLink).into(web);
+
     }
 }
